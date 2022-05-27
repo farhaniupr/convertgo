379a82b6f9b2211825b34208fdd6eb60b6c0cca4
farhaniupr <farhaniupr@gmail.com>
2022-05-27 13:25:38 +0700

    initial

---
+++ convert.go
@@ -0,0 +1,23 @@
+package convert
+
+import "fmt"
+
+func ItString(value interface{}) string {
+	if value == nil {
+		return ""
+	} else {
+		data := fmt.Sprintf("%v", value)
+		return data
+	}
+
+}
+
+func ItInt(i interface{}) interface{} {
+
+	if i == nil {
+		return 0
+	} else {
+
+		return i
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module convertgo
+
+go 1.18
