8f65acd52a94797fe053fd2bfaec6f2a8b43a7c6
farhaniupr <farhaniupr@gmail.com>
2022-05-30 14:00:18 +0700

    remove useless line

--- convert_test.go
+++ convert_test.go
@@ -1,20 +1,18 @@
 package convertgo_test
 
 import (
-	"log"
 	"testing"
 
 	"github.com/farhaniupr/convertgo"
 )
 
-// interface{} to string
+// go test -v
+
 func TestItString(t *testing.T) {
 	value_testing := "data"
 
 	result := convertgo.ItString(value_testing)
 	result_testing := convertgo.ItString(nil)
-
-	log.Println(result_testing)
 
 	if result != "data" {
 		t.Fatalf("error")
