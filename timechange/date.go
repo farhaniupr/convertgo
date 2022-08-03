688227a394df7da86e8760112098d6a1de93ed8c
farhaniupr <farhaniupr@gmail.com>
2022-08-04 00:29:27 +0700

    update interfface utc to gmt+7

--- timechange/date.go
+++ timechange/date.go
@@ -1,9 +1,24 @@
 package timechange
 
-import "github.com/golang-module/carbon"
+import (
+	"time"
+
+	"gitee.com/go-package/carbon"
+)
 
 // using carbon
 func PostDateString(target_time string) string {
 	result := carbon.Parse(target_time).DiffForHumans()
 	return result
+}
+
+func InterfaceUTCtoGMT7(target interface{}) string {
+
+	t := target.(time.Time)
+
+	loc, _ := time.LoadLocation("Asia/Jakarta")
+
+	t = t.In(loc)
+
+	return t.Format("2006-01-02 15:04:02")
 }
